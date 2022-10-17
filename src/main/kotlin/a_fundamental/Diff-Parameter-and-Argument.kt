7aba738ba92c2d2ea6205e150113ba95bb3100ca
Jonry Simbolon <jonrysimbolon19@gmail.com>
2022-10-17 14:06:56 +0700

    add Diff-Parameter-and-Argument.kt

---
+++ src/main/kotlin/a_fundamental/Diff-Parameter-and-Argument.kt
@@ -0,0 +1,12 @@
+package a_fundamental
+
+fun main() {
+    // argument 4 dan 6
+    val c = add(4, 6)
+    println(c)
+}
+
+// parameter a dan b
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
